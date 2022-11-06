36611062191044a9a02f60b587401c918a95c0ba
DT1220 <104799954+DT1220@users.noreply.github.com>
2022-11-06 17:26:39 -0600

    updated files

---
+++ Client/src/components/DashboardNewGoalPage.js
@@ -0,0 +1,11 @@
+
+function DashboardPage(){
+    return(<h1>This is DashboardNewGoalPage</h1>)
+
+
+
+
+
+
+  }
+  export default DashboardPage;
--- Client/src/components/NewGoals.js
+++ Client/src/components/NewGoals.js
@@ -6,43 +6,51 @@ import Card from 'react-bootstrap/Card';
 import Dropdown from 'react-bootstrap/Dropdown';
 
 
-
 export default function NewGoals() {
 
 
-    return (
-        <>
-        <Card>
-            <Card.Text>
-                Goal Name: 
-                <input className='box' type="text" name="name" />
-            </Card.Text>
-            <Card.Text>
-                <FaRegMoneyBillAlt/> Amount to Goal: 
-                <input className='box' type="number" name="name" />
-            </Card.Text>
-            <Card.Text>
-                <BsPiggyBank/>: 
-                <input className='box' type="number" name="name" />
-            </Card.Text>
-            <Card.Text>
-                Input Time Frame 
-                <input className='box' type="number" name="name" />
-            </Card.Text>
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Dropdown Button
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Weeks</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Months</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Years</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-            <Link to='/allgoals/true'>
-                <button>Submit Goal</button>
+  return (
+<>
+            <Link to='/'>
+                <button>Log out</button>
             </Link>
-        </Card>
-        </>
+
+    <Card>
+        <Card.Text>
+        Goal Name: 
+        <input className='box' type="text" name="name" />
+        </Card.Text>
+        <Card.Text>
+        <FaRegMoneyBillAlt/> Amount to Goal: 
+        <input className='box' type="number" name="name" />
+        </Card.Text>
+        <Card.Text>
+        <BsPiggyBank/>: 
+        <input className='box' type="number" name="name" />
+        </Card.Text>
+        <Card.Text>
+        Input Time Frame 
+        <input className='box' type="number" name="name" />
+        </Card.Text>
+        <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Dropdown Button
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Weeks</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Months</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Years</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+
+        <Link to='/allgoals'>
+            <button>Submit Goal</button>
+        </Link>
+
+    </Card>
+
+
+</>
   )
 }
